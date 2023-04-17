0ef3a75b651eb0c050dc145dc3d2f50698356fa6
test <5123250+M0nteCarl0@users.noreply.github.com>
2023-04-17 10:52:31 +0300

    Update portfolio_app.py

--- portfolio_app.py
+++ portfolio_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express
 
+st.write("Header of portfolio")
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
