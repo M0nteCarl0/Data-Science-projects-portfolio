217dc2761761e821c53c19d5fcd21107ae959393
test <5123250+M0nteCarl0@users.noreply.github.com>
2023-04-17 14:22:13 +0300

    add some template

--- portfolio_app.py
+++ portfolio_app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express
 
+st.set_page_config(
+    page_title="Main portfolio ",
+    page_icon="👋",
+)
+
 st.write("Header of portfolio")
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
